0d49e315114720a86875e798af98af85bbaf0a3c
Amit Machhiwal <amit.machhiwal@ibm.com>
2023-01-27 13:13:33 +0530

    Added a comment for ripgrep installation for the live-grep to work

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,4 @@
+-- dnf install ripgrep => Do this for the live grep to work
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
