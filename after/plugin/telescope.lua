5cb20aeac66fa0a854991f7c1cdc19a43e6c4e9e
Amit Machhiwal <amit.machhiwal@ibm.com>
2023-02-13 14:33:03 +0530

    Update package installation info

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 -- dnf install ripgrep => Do this for the live grep to work
+-- dnf install npm => To install node.js. This facilitates the plugin installation
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
