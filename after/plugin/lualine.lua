a11c275441308f0bcbf56e12caf7e05054ec7042
Amit Machhiwal <amachhiw@gmail.com>
2023-03-20 13:26:52 +0530

    Lualine configuration tweaks for a better view

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -74,11 +74,42 @@ require('lualine').setup {
     lualine_c = {
         {
             'filename',
-            file_status = true, -- displays file status (readonly status, modified status)
-            path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            file_status = true,      -- Displays file status (readonly status, modified status)
+            newfile_status = false,  -- Display new file status (new file means no write after created)
+            path = 2,                -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+
+            shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+            -- for other components. (terrible name, any suggestions?)
+            symbols = {
+                modified = '[+]',      -- Text to show when the file is modified.
+                readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                newfile = '[New]',     -- Text to show for newly created file before first write
+            }
         }
     },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {
+        'encoding', 
+        {
+            'fileformat', 
+            symbols = {
+                unix = '', -- e712
+                dos = '',  -- e70f
+                mac = '',  -- e711
+            }
+        },
+        { 
+            'filetype',
+            colored = true,   -- Displays filetype icon in color if set to true
+            icon_only = false, -- Display only an icon for filetype
+            icon = { align = 'right' }, -- Display filetype icon on the right hand side
+            -- icon =    {'X', align='right'}
+            -- Icon string ^ in table is ignored in filetype component
+        }
+    },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
